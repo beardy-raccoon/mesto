d3f9033db0c533e6c6824aff1e8a8b9881e7165c
Vlad Meshcherinov <vladislav1310@gmail.com>
2022-01-25 16:41:36 +0400

    bug-fix: fix review comments

--- script/index.js
+++ script/index.js
@@ -1,40 +1,41 @@
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 let profileEditButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
-let popupForm = document.querySelector('.popup__form');
+let popupCloseButton = popup.querySelector('.popup__close-button');
+let popupForm = popup.querySelector('.popup__form');
+let nameInput = popup.querySelector('.popup__input_name');
+let aboutInput = popup.querySelector('.popup__input_about');
+
+
 
 function openPopup() {
+  nameInput.value = profileTitle.textContent;
+  aboutInput.value = profileSubtitle.textContent;
   popup.classList.add('popup_opened');
-  popupForm.classList.add('popup__form_opened');
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
-  popupForm.classList.remove('popup__form_opened');
+
 }
-
-profileEditButton.addEventListener('click', openPopup);
-
-popupCloseButton.addEventListener('click', closePopup);
-
-popup.addEventListener('click', function (e) {
-  if (e.target === e.currentTarget) {
-    closePopup();
-  }
-});
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let aboutInput = document.querySelector('.popup__about');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = aboutInput.value;
   popup.classList.remove('popup_opened');
-  popupForm.classList.remove('popup__form_opened');
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+profileEditButton.addEventListener('click', openPopup);
+
+popupCloseButton.addEventListener('click', closePopup);
+
+popupForm.addEventListener('submit', formSubmitHandler);
+
+// Закрытие модального окна по клику вне его области
+/*popup.addEventListener('click', function (e) {
+  if (e.target === e.currentTarget) {
+    closePopup();
+  }
+});*/
