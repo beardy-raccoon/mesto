64a3474565d4e2325a0ec5659a658f4f274e3b7a
Vlad Meshcherinov <vladislav1310@gmail.com>
2022-03-29 00:54:15 +0400

    feature: replace initCards with memes, modify del btn

--- utils/initialCards.js
+++ utils/initialCards.js
@@ -1,26 +1,26 @@
 export const initialCards = [
   {
-    name: 'Саратов',
-    link: './images/saratov.jpeg'
+    name: 'Oh not again',
+    link: 'https://i0.wp.com/tutorial.eyehunts.com/wp-content/uploads/2021/07/257124479_4492885784093598_7332326531376746242_n.jpg?resize=750%2C769&ssl=1'
   },
   {
-    name: 'Гудаури',
-    link: './images/gudauri.jpeg'
+    name: 'For sure',
+    link: 'https://i0.wp.com/tutorial.eyehunts.com/wp-content/uploads/2021/07/273193242_481486090007616_4906080216823475087_n.jpg'
   },
   {
-    name: 'Венеция',
-    link: './images/venice.jpeg'
+    name: 'Oopps',
+    link: 'https://media-exp1.licdn.com/dms/image/C4D22AQHDmkpnBj9xhg/feedshare-shrink_1280/0/1647508160547?e=1651104000&v=beta&t=zKl0BQabrvFfUy9QD0oi2fHxZVTIBiMg0mo3Um3hzcI'
   },
   {
-    name: 'Куршская коса',
-    link: './images/kosa.jpeg'
+    name: 'I be like',
+    link: 'https://i0.wp.com/tutorial.eyehunts.com/wp-content/uploads/2021/07/208073311_1453256311692332_8313631719648907675_n.jpg?w=1080&ssl=1'
   },
   {
-    name: 'Мурманск',
-    link: './images/murmansk.jpg'
+    name: 'Sad but true',
+    link: 'https://i0.wp.com/tutorial.eyehunts.com/wp-content/uploads/2021/07/243849668_4351940474854797_3811414998141063960_n.jpg'
   },
   {
-    name: 'Платформа Псырцха',
-    link: './images/psircha.jpeg'
+    name: 'Mem of the day',
+    link: 'https://media-exp1.licdn.com/dms/image/C4D22AQEb5rMFr0U1xg/feedshare-shrink_800/0/1647501575144?e=1651104000&v=beta&t=EV3bGDevlSzxq7mScV8L3od-pN2cFDUkHa_X3yAO4oY'
   },
 ];
